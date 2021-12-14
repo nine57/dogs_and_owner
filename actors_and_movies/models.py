7fe5161ac63c3a1ebc86c3e7701ff5c6fd9d197c
nine57 <gusrn015@gmail.com>
2021-12-14 20:09:33 +0900

    ADD

--- actors_and_movies/models.py
+++ actors_and_movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.fields import DateField
 
-# Create your models here.
+# models here.
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=45)
+    lsat_name = models.CharField(max_length=45)
+    date_of_birth = models.DateField()
