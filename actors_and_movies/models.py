5418795af644c24a56a099c7b2bcc4d661913d86
nine57 <gusrn015@gmail.com>
2021-12-15 22:00:34 +0900

    ADD: View, model, DB set-up

--- actors_and_movies/models.py
+++ actors_and_movies/models.py
@@ -4,6 +4,25 @@ from django.db.models.fields import DateField
 # models here.
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=45)
-    lsat_name = models.CharField(max_length=45)
+    first_name    = models.CharField(max_length=45)
+    last_name     = models.CharField(max_length=45)
     date_of_birth = models.DateField()
+    movie         = models.ManyToManyField("Movie", through='Actor_movie')
+
+    class Meta:
+        db_table = 'actors'
+
+class Movie(models.Model):
+    title        = models.CharField(max_length=45)
+    release_date = models.DateField()
+    running_time = models.IntegerField()
+
+    class Meta:
+        db_table = 'movies'
+
+class Actor_movie(models.Model):
+    actor = models.ForeignKey("Actor", on_delete=models.CASCADE)
+    movie = models.ForeignKey("Movie", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'actors_movies'
